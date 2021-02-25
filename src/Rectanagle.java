afcf79b912d331357b73243fc141fa72509e7322
sjoshi-1 <200455953@student.georgianc.on.ca>
2021-02-25 12:28:44 -0500

    Initial commit

---
+++ src/Rectanagle.java
@@ -0,0 +1,34 @@
+public class Rectanagle {
+    private int length,width;
+
+
+
+    public Rectanagle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+    
+    public int getPerimeter(){
+        return 0;
+    }
+
+    public int getArea(){
+        return 0;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
